a6122722df4f7008248534e6179fce7928877ff5
xelloss120 <xelloss120@hotmail.com>
2022-05-27 00:11:25 +0900

    表情変更対応

---
+++ Assets/Script/ChangeBlendShape.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using VRM;
+
+public class ChangeBlendShape : MonoBehaviour
+{
+    [SerializeField] ImportVRM ImportVRM;
+    [SerializeField] Dropdown Dropdown;
+
+    public void Changed()
+    {
+        var proxy = ImportVRM.Model.GetComponent<VRMBlendShapeProxy>();
+        switch (Dropdown.value)
+        {
+            case 0:
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Joy), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Angry), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Sorrow), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Fun), 0);
+                break;
+            case 1:
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Joy), 1);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Angry), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Sorrow), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Fun), 0);
+                break;
+            case 2:
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Joy), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Angry), 1);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Sorrow), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Fun), 0);
+                break;
+            case 3:
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Joy), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Angry), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Sorrow), 1);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Fun), 0);
+                break;
+            case 4:
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Joy), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Angry), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Sorrow), 0);
+                proxy.AccumulateValue(BlendShapeKey.CreateFromPreset(BlendShapePreset.Fun), 1);
+                break;
+        }
+        proxy.Apply();
+    }
+}
