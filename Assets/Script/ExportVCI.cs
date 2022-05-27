b5a060bd98c155e18566cc5137f89317be634762
xelloss120 <xelloss120@hotmail.com>
2022-05-27 23:20:50 +0900

    ラグドール書き出し対応

--- Assets/Script/ExportVCI.cs
+++ Assets/Script/ExportVCI.cs
@@ -6,6 +6,7 @@ using UniGLTF;
 using VRM;
 using VCI;
 using VRMShaders;
+using RootMotion.Dynamics;
 
 public class ExportVCI : MonoBehaviour
 {
@@ -36,11 +37,11 @@ public class ExportVCI : MonoBehaviour
 
         if (path == "") return;
 
-        var doll = Instantiate(RagDollPrefab);
+        var root = Instantiate(RagDollPrefab);
         var model = VRMBoneNormalizer.Execute(ImportVRM.Model, false); // 表情適用のため再正規化
-        model.transform.parent = doll.transform;
+        model.transform.parent = root.transform;
 
-        var vci = doll.GetComponent<VCIObject>();
+        var vci = root.GetComponent<VCIObject>();
         vci.Meta.title = Title.text;
         vci.Meta.version = Version.text;
         vci.Meta.author = Author.text;
@@ -50,19 +51,76 @@ public class ExportVCI : MonoBehaviour
         vci.Meta.modelDataLicenseType = (VciMetaLicenseType)LicenseType.value;
         vci.Meta.modelDataOtherLicenseUrl = LicenseUrl.text;
 
-        var sub = model.AddComponent<VCISubItem>();
-        var rig = model.AddComponent<Rigidbody>();
-        rig.isKinematic = true;
+        var anim = model.GetComponent<Animator>();
+        BipedRagdollReferences bipedRagdollReferences;
+        bipedRagdollReferences.root = model.transform;
+        bipedRagdollReferences.hips = anim.GetBoneTransform(HumanBodyBones.Hips);
+        bipedRagdollReferences.spine = anim.GetBoneTransform(HumanBodyBones.Spine);
+        bipedRagdollReferences.chest = anim.GetBoneTransform(HumanBodyBones.Chest);
+        bipedRagdollReferences.head = anim.GetBoneTransform(HumanBodyBones.Head);
+        bipedRagdollReferences.leftUpperLeg = anim.GetBoneTransform(HumanBodyBones.LeftUpperLeg);
+        bipedRagdollReferences.leftLowerLeg = anim.GetBoneTransform(HumanBodyBones.LeftLowerLeg);
+        bipedRagdollReferences.leftFoot = anim.GetBoneTransform(HumanBodyBones.LeftFoot);
+        bipedRagdollReferences.rightUpperLeg = anim.GetBoneTransform(HumanBodyBones.RightUpperLeg);
+        bipedRagdollReferences.rightLowerLeg = anim.GetBoneTransform(HumanBodyBones.RightLowerLeg);
+        bipedRagdollReferences.rightFoot = anim.GetBoneTransform(HumanBodyBones.RightFoot);
+        bipedRagdollReferences.leftUpperArm = anim.GetBoneTransform(HumanBodyBones.LeftUpperArm);
+        bipedRagdollReferences.leftLowerArm = anim.GetBoneTransform(HumanBodyBones.LeftLowerArm);
+        bipedRagdollReferences.leftHand = anim.GetBoneTransform(HumanBodyBones.LeftHand);
+        bipedRagdollReferences.rightUpperArm = anim.GetBoneTransform(HumanBodyBones.RightUpperArm);
+        bipedRagdollReferences.rightLowerArm = anim.GetBoneTransform(HumanBodyBones.RightLowerArm);
+        bipedRagdollReferences.rightHand = anim.GetBoneTransform(HumanBodyBones.RightHand);
+        var options = BipedRagdollCreator.Options.Default;
+        options.weight = 40;
+        BipedRagdollCreator.Create(bipedRagdollReferences, options);
+
+        var joints = root.GetComponentsInChildren<ConfigurableJoint>();
+        foreach (var joint in joints) DestroyImmediate(joint);
+
+        var item = model.AddComponent<VCISubItem>();
+        item.Grabbable = true;
+        item.GroupId = 1;
+
+        SetJoint(anim, HumanBodyBones.Chest, HumanBodyBones.Hips, root.transform, new Vector3(1, 0, 0), -45, 45);
+        SetJoint(anim, HumanBodyBones.Head, HumanBodyBones.Chest, root.transform, new Vector3(1, 0, 0), -90, 45);
+        SetJoint(anim, HumanBodyBones.LeftUpperLeg, HumanBodyBones.Hips, root.transform, new Vector3(1, 0, 0), -120, 45);
+        SetJoint(anim, HumanBodyBones.LeftLowerLeg, HumanBodyBones.LeftUpperLeg, root.transform, new Vector3(1, 0, 0), 0, 140);
+        SetJoint(anim, HumanBodyBones.LeftFoot, HumanBodyBones.LeftLowerLeg, root.transform, new Vector3(1, 0, 0), -45, 90);
+        SetJoint(anim, HumanBodyBones.RightUpperLeg, HumanBodyBones.Hips, root.transform, new Vector3(1, 0, 0), -120, 45);
+        SetJoint(anim, HumanBodyBones.RightLowerLeg, HumanBodyBones.RightUpperLeg, root.transform, new Vector3(1, 0, 0), 0, 140);
+        SetJoint(anim, HumanBodyBones.RightFoot, HumanBodyBones.RightLowerLeg, root.transform, new Vector3(1, 0, 0), -45, 90);
+        SetJoint(anim, HumanBodyBones.LeftUpperArm, HumanBodyBones.Chest, root.transform, new Vector3(0, 0, 1), -80, 80);
+        SetJoint(anim, HumanBodyBones.LeftLowerArm, HumanBodyBones.LeftUpperArm, root.transform, new Vector3(0, -1, 0), -140, 0);
+        SetJoint(anim, HumanBodyBones.LeftHand, HumanBodyBones.LeftLowerArm, root.transform, new Vector3(0, 0, 1), -80, 80);
+        SetJoint(anim, HumanBodyBones.RightUpperArm, HumanBodyBones.Chest, root.transform, new Vector3(0, 0, 1), -80, 80);
+        SetJoint(anim, HumanBodyBones.RightLowerArm, HumanBodyBones.RightUpperArm, root.transform, new Vector3(0, 1, 0), -140, 0);
+        SetJoint(anim, HumanBodyBones.RightHand, HumanBodyBones.RightLowerArm, root.transform, new Vector3(0, 0, 1), -80, 80);
 
         var data = new ExportingGltfData();
         var exporter = new VCIExporter(data);
-        exporter.Prepare(doll);
+        exporter.Prepare(root);
         exporter.Export(new RuntimeTextureSerializer());
         exporter.Dispose();
         var bytes = data.ToGlbBytes();
         File.WriteAllBytes(path, bytes);
 
-        Destroy(doll);
+        Destroy(root);
+    }
+
+    void SetJoint(Animator anim, HumanBodyBones bone, HumanBodyBones body, Transform root, Vector3 axis, float min, float max)
+    {
+        var transform = anim.GetBoneTransform(bone);
+        transform.parent = root;
+
+        var item = transform.gameObject.AddComponent<VCISubItem>();
+        item.Grabbable = true;
+        item.GroupId = 1;
+
+        var joint = transform.gameObject.AddComponent<HingeJoint>();
+        joint.connectedBody = anim.GetBoneTransform(body).gameObject.GetComponent<Rigidbody>();
+        joint.useLimits = true;
+        joint.axis = axis;
+        joint.limits = new JointLimits() { min = min, max = max };
     }
 
     /// <summary>
